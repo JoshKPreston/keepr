a78bf98776ec52d54e5ddd50fa29a3ace7acfe0c
Josh K Preston <kanemetal@gmail.com>
2021-01-02 01:59:51 -0700

    'init'

---
+++ client/src/AuthConfig.js
@@ -0,0 +1,3 @@
+export const domain = 'bcwclassroom.us.auth0.com'
+export const clientId = 'csgstdwTeKjob32P31fW3JpxK7995Mwp'
+export const audience = 'https://keepr/api'
---
+++ client/src/router.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import { authGuard } from '@bcwdev/auth0provider-client'
+
+function loadPage(page) {
+  return () => import(`./pages/${page}.vue`)
+}
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: loadPage('HomePage')
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: loadPage('ProfilePage'),
+    beforeEnter: authGuard
+  },
+  {
+    path: '/vault/:id',
+    name: 'Vault',
+    component: loadPage('VaultPage')
+  }
+]
+
+const router = createRouter({
+  linkActiveClass: 'router-link-active',
+  linkExactActiveClass: 'router-link-exact-active',
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
